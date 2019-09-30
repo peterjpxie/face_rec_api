ca21765ad96aebdd7d8a96a62ae6d82bc740c79c
Peter Jiping Xie <peter.jp.xie@gmail.com>
2019-09-30 20:32:07 +0800

    update

--- flask_server.py
+++ flask_server.py
@@ -22,17 +22,18 @@ def hello_world():
 ''' file uploads
 http://flask.palletsprojects.com/en/1.0.x/patterns/fileuploads/
 ''' 
-UPLOAD_FOLDER = 'TestFiles'
-ALLOWED_EXTENSIONS = set(['txt', 'py', 'png', 'jpg', 'jpeg', 'gif', 'ipynb'])
+UPLOAD_FOLDER = 'image_files'
+ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
+# ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
                                
 @app.route('/face_recognition', methods=['POST', 'GET'])
-def test_upload_file():
+def face_recognition():
     if request.method == 'POST':
         # print(request.get_data().decode('utf-8'))
         # check if the post request has the file part
@@ -46,68 +47,25 @@ def test_upload_file():
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
+            # get parameters from url if any
+            param_features = request.args.get('features', '')
             # read file content directly. Note once read, the file becomes empty when try to save later. Cursor issue, IMO.
             # print('file content:\n', file.read()) 
             filename = secure_filename(file.filename) # secure_filename converts to a secure filename.
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # redirect the user to url_for('uploaded_file', filename=filename), that is, /uploads/filename, 
-            # to return uploaded file to user.
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
             # return redirect(url_for('uploaded_file', filename=filename))
-            return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+            # return send_from_directory(UPLOAD_FOLDER, filename)
+            if param_features.lower() == 'true':
+                pass # to add logic
+            resp_data = {'ID':'', 'name': 'unknown',
+                        'features': {}
+                        }
+            return json.dumps(resp_data)
+            
     return '''
     <!doctype html>
     <title>Face Recognition</title>
     <h1>Upload a image</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
-
-""" request body example:
-------WebKitFormBoundaryOPd4j88kbDMQXRtm
-Content-Disposition: form-data; name="file"; filename="Chinese.txt"
-Content-Type: text/plain
-
-你好
-
-------WebKitFormBoundaryOPd4j88kbDMQXRtm--
-"""
-
-@app.route('/uploaded/<filename>')
-def uploaded_file(filename):
-    # send_from_directory is similar to send_file
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
-# File upload example ends.
-
-@app.route('/face_match', methods=['POST', 'GET'])
-def test_upload_file():
-    if request.method == 'POST':
-        # print(request.get_data().decode('utf-8'))
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file'] # 'file' is the name in the input request, could be file1, file2.
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            # read file content directly. Note once read, the file becomes empty when try to save later. Cursor issue, IMO.
-            # print('file content:\n', file.read()) 
-            filename = secure_filename(file.filename) # secure_filename converts to a secure filename.
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # redirect the user to url_for('uploaded_file', filename=filename), that is, /uploads/filename, 
-            # to return uploaded file to user.
-            # return redirect(url_for('uploaded_file', filename=filename))
-            return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
     <form method=post enctype=multipart/form-data>
       <input type=file name=file>
       <input type=submit value=Upload>
