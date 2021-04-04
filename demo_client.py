aec0d2d311bd50aff081ea34c2777b6687b43b8f
Peter Jiping Xie <peter.jp.xie@gmail.com>
2021-04-04 15:54:13 +1000

    Daily update

--- demo_client.py
+++ demo_client.py
@@ -4,22 +4,22 @@ import json
 def test_face_match():
     url = 'http://127.0.0.1:5001/face_match'
     # open file in binary mode
-    files = {'file1': open('sample_images/peter.jpg', 'rb'),
-             'file2': open('sample_images/peter2.jpg', 'rb')}     
+    files = {'file1': open('sample_images/obama.jpg', 'rb'),
+             'file2': open('sample_images/obama2.jpg', 'rb')}     
     resp = requests.post(url, files=files)
     print( 'face_match response:\n', json.dumps(resp.json()) )
     
 def test_face_rec():
     url = 'http://127.0.0.1:5001/face_rec'
     # open file in binary mode
-    files = {'file': open('sample_images/peter2.jpg', 'rb')} 
+    files = {'file': open('sample_images/obama2.jpg', 'rb')} 
     params = {'facial_features': 'true', 'face_locations':'true'}
     resp = requests.post(url, files = files, params = params)
     print( 'face_rec response:\n', json.dumps(resp.json()) )
     
 def main():
-    # test_face_match()
-    test_face_rec()
+    test_face_match()
+    # test_face_rec()
     
 if __name__ == '__main__':
     main()
--- flask_server.py
+++ flask_server.py
@@ -1,4 +1,4 @@
-# from flask import Flask, request, render_template, flash, redirect, url_for
+from flask import Flask, request, render_template, flash, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
 import json
